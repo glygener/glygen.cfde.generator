ca2b15a562829a724c0c9f706cb95b389cc8ff48
Rene Ranzinger <rene@ccrc.uga.edu>
2022-06-13 00:16:42 -0400

    Adding ignore files for proteins and glycans

--- src/main/java/org/glygen/cfde/generator/csv/GlycanFilter.java
+++ src/main/java/org/glygen/cfde/generator/csv/GlycanFilter.java
@@ -1,6 +1,9 @@
 package org.glygen.cfde.generator.csv;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.List;
 
 public class GlycanFilter
 {
@@ -8,12 +11,23 @@ public class GlycanFilter
 
     public GlycanFilter()
     {
-        // this.m_ignore.add("G60070LT");
-        // this.m_ignore.add("G78166NF");
+        try
+        {
+            List<String> t_allLines = Files.readAllLines(Paths.get("./data/glycan.ignore"));
+            for (String t_line : t_allLines)
+            {
+                this.m_ignore.add(t_line);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public boolean isIgnore(String a_id)
     {
         return this.m_ignore.contains(a_id);
     }
+
 }
