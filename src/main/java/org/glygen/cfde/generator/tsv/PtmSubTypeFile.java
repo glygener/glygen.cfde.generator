6433875dfb417acba56017b67236e4a9125f5aab
Rene Ranzinger <rene@ccrc.uga.edu>
2025-08-16 02:04:46 -0400

    Minor updates and documentation
    Added a comment for the -x flag and remove the static lines that have been added to ptm_subtype.tsv

--- src/main/java/org/glygen/cfde/generator/tsv/PtmSubTypeFile.java
+++ src/main/java/org/glygen/cfde/generator/tsv/PtmSubTypeFile.java
@@ -10,13 +10,6 @@ public class PtmSubTypeFile extends TSVFile
         super();
         this.m_header = new String[] { "id", "name", "description", "synonyms" };
         this.openFile(a_folderPath + File.separator + "ptm_subtype.tsv");
-
-        // static file entries
-        this.write("GO:0006486", "protein glycosylation",
-                "A protein modification process that results in the addition of a carbohydrate or carbohydrate derivative unit to a protein amino acid, e.g., the addition of glycan chains to proteins. protein phosphorylation – The process of introducing a phosphate group onto a protein.",
-                null);
-        this.write("GO:0006468", "protein phosphorylation",
-                "The process of introducing a phosphate group on to a protein.", null);
     }
 
     public void write(String a_id, String a_name, String a_description, String a_synonyms)
--- src/run/java/org/glygen/cfde/generator/AppRun.java
+++ src/run/java/org/glygen/cfde/generator/AppRun.java
@@ -6,7 +6,8 @@ public class AppRun
 {
     public static void main(String[] args) throws IOException
     {
-        String t_comandLine = "-c ./data/files.csv -o ./data/output/ -p ./data/glygen.properties -m ./data/mapping/ -g";
+        // -x exclude array
+        String t_comandLine = "-c ./data/files.csv -o ./data/output/ -p ./data/glygen.properties -m ./data/mapping/ -g -x";
         String[] t_args = t_comandLine.split(" ");
         App.main(t_args);
     }
